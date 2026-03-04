7c134791927dc8112ee0bb4bbadf0a3b7a68bf3e
Vivek N Shetty <vivek.n.shetty.24@gmail.com>
2026-03-04 15:38:59 +0530

    Add files via upload

---
+++ intro/student_details.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+     char str1[]="Vivek";
+     int year=2;
+     char str2[]="01SU24CS171";
+     float cgpa=8.53;
+  printf("Student Name:%s\n",str1);
+  printf("Studying Year : %d nd\n",year);
+  printf("USN:%s\n",str2);
+  printf("CGPA:%.2f",cgpa);
+}
