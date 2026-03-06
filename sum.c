a4e8fec75652227c6a434d89b4bbb65ea2bb36c2
Vivek N Shetty <vivek.n.shetty.24@gmail.com>
2026-03-06 07:23:29 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+     int num1,num2,sum;
+     printf("Enter The First Number :");
+     scanf("%d",&num1);
+     printf("Enter the Second Number :");
+     scanf("%d",&num2);
+     sum=num1+num2;
+     printf("%d + %d = %d\n",num1,num2,sum);
+     printf("The sum of %d and %d is %d",num1,num2,sum);
+     return 0;
+}
