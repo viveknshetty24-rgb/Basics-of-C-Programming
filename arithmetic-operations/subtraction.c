51a6269e179536a562e3f40c78054144ca5fcb7f
Vivek N Shetty <vivek.n.shetty.24@gmail.com>
2026-03-07 20:47:39 +0530

    Add files via upload

---
+++ arithmetic-operations/subtraction.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+     int a,b;
+     printf("Enter the first number:");
+     scanf("%d", &a);
+     printf("Enter the second number:");
+     scanf("%d", &b);
+     printf("%d - %d = %d",a,b,a-b);
+     return 0;
+}
