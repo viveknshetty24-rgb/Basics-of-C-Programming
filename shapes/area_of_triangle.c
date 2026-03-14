52e6a7f314f9a554ad90952fb0805c7575b230d4
Vivek N Shetty <vivek.n.shetty.24@gmail.com>
2026-03-14 20:32:56 +0530

    Update prompts for triangle area calculation

--- shapes/area_of_triangle.c
+++ shapes/area_of_triangle.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main(){
      float base,height,area;
-     printf("Enter the base:");
+     printf("Enter the Base:");
      scanf("%f",&base);
-     printf("Enter the height:");
+     printf("Enter the Height:");
      scanf("%f",&height);
      area=0.5*base*height;
-     printf("The Area of the triangle is:%.2f",area);
+     printf("The Area of the Triangle is:%.2f",area);
      return 0;
 }
