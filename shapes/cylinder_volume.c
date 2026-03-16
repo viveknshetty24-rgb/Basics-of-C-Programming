e10831b3c582d993f2e9255a0fd6c5dadeb213bf
Vivek N Shetty <vivek.n.shetty.24@gmail.com>
2026-03-16 18:04:13 +0530

    Add files via upload

---
+++ shapes/cylinder_volume.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+     int r = 5;
+     int height=10;
+     printf("The volume of cylinder with radius %d and height %d is %f", r,height,3.14*r*r*height);
+     return 0;
+}
