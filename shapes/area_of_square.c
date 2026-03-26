1ba045e4fea6f13aafdf851bf4f439086f4c7923
Vivek N Shetty <vivek.n.shetty.24@gmail.com>
2026-03-26 21:17:19 +0530

    Add files via upload

---
+++ shapes/area_of_square.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+     int side,area;
+     printf("Enter the Side of The Square : ");
+     scanf("%d",&side);
+     area=side*side;
+     printf("The area of the Square is : %d",area);
+     return 0;
+}
