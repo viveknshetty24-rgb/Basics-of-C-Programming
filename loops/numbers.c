894a91e6ef403f4f3f4187b7e305fc368b7ca20c
Vivek N Shetty <vivek.n.shetty.24@gmail.com>
2026-03-28 16:45:52 +0530

    Add files via upload

---
+++ loops/numbers.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+     int i=1,n=10;
+     while(i<=n){
+          printf("%d\t",i);
+          i++;
+     }
+     return 0;
+}
---
+++ loops/numbers_rev.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+     int i=10,n=1;
+     while(i>=n){
+          printf("%d\t",i);
+          i--;
+     }
+     return 0;
+}
