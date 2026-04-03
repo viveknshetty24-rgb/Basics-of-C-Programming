50c383ebdeb6c57be50cdc52decc2c45b82a6c77
Vivek N Shetty <vivek.n.shetty.24@gmail.com>
2026-04-03 16:05:33 +0530

    Add files via upload

---
+++ loops/odd_sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+     int n, sum = 0;
+     printf("Enter the number length:");
+     scanf("%d", &n);
+     for (int i = 0; i < n; i++)
+     {
+          printf(" %d", i);
+          sum = sum + i;
+     }
+     printf("\nThe Sum of Odd Numbers is %d", sum);
+}
