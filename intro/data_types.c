9d3633c952ac508d494c8ec69ff6b7591e357b6e
Vivek N Shetty <vivek.n.shetty.24@gmail.com>
2026-04-11 19:37:43 +0530

    Add files via upload

---
+++ intro/data_types.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+     int a=5;
+     float b=4.2;
+     char str[]="vivek";
+     double c=2.133131;
+     char d='A';
+     printf("\nint = %d",a);
+     printf("\nfloat = %f",b);
+     printf("\nString = %s",str);
+     printf("\ndouble = %lf",c);
+     printf("\nchar=%c",d);
+     return 0;
+}
