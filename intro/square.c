4065504deb359681fc23e507c3e22574bc3ea8dd
Vivek N Shetty <vivek.n.shetty.24@gmail.com>
2026-04-27 12:12:26 +0530

    Add files via upload

---
+++ intro/square.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+     int n, square;
+     printf("\n Enter a number:");
+     scanf("%d", &n);
+     square = n * n;
+     printf("Square of %d is %d", n, square);
+     return 0;
+}
