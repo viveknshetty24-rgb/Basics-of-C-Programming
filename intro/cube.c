22b17ad27a60fdcc2e95418f18929124d1094dbd
Vivek N Shetty <vivek.n.shetty.24@gmail.com>
2026-04-29 15:16:24 +0530

    Add files via upload

---
+++ intro/cube.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+     int n,cube;
+     printf("\n Enter a number:");
+     scanf("%d",&n);
+     cube=n*n*n;
+     printf("\n The Cube of %d is %d",n,cube);
+     return 0;
+}
