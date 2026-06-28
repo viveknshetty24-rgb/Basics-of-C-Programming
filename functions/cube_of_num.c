674a143277d36220167945a51507fe1420805a6a
Vivek N Shetty <vivek.n.shetty.24@gmail.com>
2026-06-28 15:50:35 +0530

    Update and rename cube_of_num.c to functions/cube_of_num.c

--- functions/cube_of_num.c
+++ functions/cube_of_num.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-int square(int n){
+int cube(int n){
      return n*n*n;
 }
 int main(){
      int a;
      printf("\nEnter a number:\n");
      scanf("%d",&a);
-     printf("\nCube of %d is %d",a,square(a));
+     printf("\nCube of %d is %d",a,cube(a));
 }
